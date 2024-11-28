a8257a6d43438d247d43f99ddea1c000dcc1dc8a
Bratu Patricia <patricia.bratu@stud.acs.upb.ro>
2024-11-28 16:35:51 +0200

    Frontend Homepage

---
+++ my_app/src/App.jsx
@@ -0,0 +1,16 @@
+import Div_Header from "./Div_Header"
+import Divwithrecipes from "./Divwithrecipes.jsx";
+import Div_contact from "./div_contact.jsx";
+import Div_bottom from "./div_bottom.jsx";
+function App() {
+  return (
+    <>
+      <Div_Header />
+      <Divwithrecipes />
+      <Div_contact />
+      <Div_bottom />
+    </>
+  );
+}
+
+export default App
