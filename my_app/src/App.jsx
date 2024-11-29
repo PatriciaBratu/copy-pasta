d3b10416e1d81123bd2b6f8552d03c007b302958
Bratu Patricia <patricia.bratu@stud.acs.upb.ro>
2024-11-29 11:04:40 +0200

    +background

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Div_Header from "./Div_Header"
+import Divwithrecipes from "./Divwithrecipes.jsx";
+import Div_contact from "./div_contact.jsx";
+import Div_bottom from "./div_bottom.jsx";
+import './App.css';
+import image from './assets/Group 91.png';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="background">
+        <img
+          src={image}
+          alt="Foreground"
+          className="foreground-image"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Div_Header />
+      <Divwithrecipes />
+      <Div_contact />
+      <Div_bottom />
     </>
   )
 }
